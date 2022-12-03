8ac2a31d36cbcbb0f0cfabea2637779440ed9250
HardGraphite <jodieydchen@gmail.com>
2022-12-03 15:31:53 +0000

    Fix problems in commit 0988b1d

--- src/utilities/thread.c
+++ src/utilities/thread.c
@@ -5,11 +5,19 @@
 #include <stdint.h>
 #include <time.h>
 
+#ifdef _MSC_VER
+#	include <compat/msvc_stdatomic.h>
+#else
+#	include <stdatomic.h>
+#endif
+
 #include <utilities/attributes.h>
 #include <utilities/pragmas.h>
+#include <utilities/thread.h>
 
 #if OW_THRD_WINNT
 #	include <Windows.h>
+#	include <process.h>
 static_assert(sizeof(ow_thrd_t) == sizeof(HANDLE), "");
 #elif OW_THRD_POSIX
 #	include <sched.h> // sched_yield()
@@ -21,16 +29,39 @@ ow_pragma_message("thread utilities are not implemeted")
 #	include <stdlib.h>
 #endif
 
-#if OW_THRD_POSIX
+#if OW_THRD_WINNT
+
+struct winnt_thrd_create_func_wrapper_arg {
+	ow_thrd_start_t func;
+	void *func_arg;
+	atomic_int ready;
+};
+
+static unsigned int __stdcall winnt_thrd_create_func_wrapper(void *arg) {
+	struct winnt_thrd_create_func_wrapper_arg *const wrapper_arg = arg;
+	const ow_thrd_start_t func = wrapper_arg->func;
+	void *const func_arg = wrapper_arg->func_arg;
+	atomic_fetch_add(&wrapper_arg->ready, 1);
+	while (atomic_load(&wrapper_arg->ready) < 2)
+		ow_thrd_yield();
+	const int res = func(func_arg);
+	return (unsigned int)res;
+}
+
+#elif OW_THRD_POSIX
 
 struct posix_thrd_create_func_wrapper_arg {
 	ow_thrd_start_t func;
 	void *func_arg;
+	volatile atomic_uint ready;
 };
 
 static void *posix_thrd_create_func_wrapper(void *arg) {
 	struct posix_thrd_create_func_wrapper_arg *const wrapper_arg = arg;
-	const int res = wrapper_arg->func(wrapper_arg->func_arg);
+	const ow_thrd_start_t func = wrapper_arg->func;
+	void *const func_arg = wrapper_arg->func_arg;
+	atomic_store(&wrapper_arg->ready, 1);
+	const int res = func(func_arg);
 	return (void *)(uintptr_t)(unsigned int)res;
 }
 
@@ -38,18 +69,25 @@ static void *posix_thrd_create_func_wrapper(void *arg) {
 
 int ow_thrd_create(ow_thrd_t *thrd, ow_thrd_start_t func, void *arg) {
 #if OW_THRD_WINNT
+	struct winnt_thrd_create_func_wrapper_arg wrapper_arg = {func, arg, 0};
 	const uintptr_t handle = _beginthreadex(
-		NULL, 0, (LPTHREAD_START_ROUTINE)func, arg, 0, NULL);
+		NULL, 0, winnt_thrd_create_func_wrapper, &wrapper_arg, 0, NULL);
 	if (!handle)
 		return ow_thrd_error;
-	*thrd = h;
+	atomic_fetch_add(&wrapper_arg.ready, 1);
+	while (atomic_load(&wrapper_arg.ready) < 2)
+		ow_thrd_yield();
+	*thrd = (HANDLE)handle;
 	return ow_thrd_success;
 #elif OW_THRD_POSIX
-	const int state = pthread_create(
-		thrd, NULL, posix_thrd_create_func_wrapper,
-		&(struct posix_thrd_create_func_wrapper_arg){func, arg}
-	);
-	return state == 0 ? ow_thrd_success : ow_thrd_error;
+	struct posix_thrd_create_func_wrapper_arg wrapper_arg = {func, arg, 0};
+	const int state =
+		pthread_create(thrd, NULL, posix_thrd_create_func_wrapper, &wrapper_arg);
+	if (state != 0)
+		return ow_thrd_error;
+	while (!atomic_load(&wrapper_arg.ready))
+		ow_thrd_yield();
+	return ow_thrd_success;
 #elif OW_THRD_STDC
 	return thrd_create(thrd, func, arg);
 #else
@@ -60,7 +98,8 @@ int ow_thrd_create(ow_thrd_t *thrd, ow_thrd_start_t func, void *arg) {
 
 int ow_thrd_equal(ow_thrd_t lhs, ow_thrd_t rhs) {
 #if OW_THRD_WINNT
-	return CompareObjectHandles(lhs, rhs);
+	const DWORD lhs_id = GetThreadId(lhs), rhs_id = GetThreadId(rhs);
+	return lhs_id != 0 && lhs_id == rhs_id;
 #elif OW_THRD_POSIX
 	return lhs == rhs;
 #elif OW_THRD_STDC
@@ -84,14 +123,14 @@ ow_thrd_t ow_thrd_current(void) {
 
 int ow_thrd_sleep(const struct timespec *duration, struct timespec *remaining) {
 #if OW_THRD_WINNT
-	const struct timespec duration_ = duration;
+	struct timespec duration_ = *duration;
 	while (duration_.tv_sec) {
 		DWORD time_sec;
 		if (duration_.tv_sec >= UINT_MAX / 1000)
 			time_sec = UINT_MAX / 1000;
 		else
 			time_sec = (DWORD)duration_.tv_sec;
-		Sleep(time_ms * 1000);
+		Sleep(time_sec * 1000);
 		duration_.tv_sec -= time_sec;
 	}
 	if (duration_.tv_nsec) {
@@ -232,16 +271,16 @@ int ow_mtx_lock(ow_mtx_t *mutex) {
 int ow_mtx_timedlock(ow_mtx_t *mutex, const struct timespec *time_point) {
 #if OW_THRD_WINNT
 	const struct timespec time_point_ = *time_point;
-	while (ow_mtx_trylock(mtx) != thrd_success) {
+	while (ow_mtx_trylock(mutex) != ow_thrd_success) {
 		struct timespec current_time;
-		timespec_get(current_time, TIME_UTC);
+		timespec_get(&current_time, TIME_UTC);
 		if (current_time.tv_sec > time_point_.tv_sec ||
 				(current_time.tv_sec == time_point_.tv_sec &&
 				current_time.tv_nsec > time_point_.tv_nsec))
-			return thrd_busy;
+			return ow_thrd_busy;
 		SwitchToThread();
 	}
-	return thrd_success;
+	return ow_thrd_success;
 #elif OW_THRD_POSIX
 	const int state = pthread_mutex_timedlock(mutex, time_point);
 	return state == 0 ? ow_thrd_success : ow_thrd_busy;
